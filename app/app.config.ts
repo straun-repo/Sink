44da8e6e1105ebc130d77ebbf7677cc6d8916e5b
straun-repo <rkapelko@googlemail.com>
2026-08-12 12:42:28 +0100

    Update app configuration for Straun Links

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Sink',
+  title: 'Straun Links',
   documentation: 'https://docs.sink.cool',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  github: 'https://github.com/straun-repo/Sink',
+  coffee: '',
+  twitter: '',
+  telegram: '',
+  description: 'Short links for Straun Group.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
