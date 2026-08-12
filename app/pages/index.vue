cfc6090d8ace22323a77c2b367af9ef995d761fd
straun-repo <rkapelko@googlemail.com>
2026-08-12 13:23:32 +0100

    Refactor index.vue layout and add content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,45 @@
+<script setup lang="ts">
+const { title, description } = useAppConfig()
+</script>
+
 <template>
-  <div
+  <section
     class="
-      flex flex-col justify-center overflow-x-clip bg-background text-foreground
+      flex flex-1 flex-col items-center justify-center bg-background px-6 py-24
+      text-foreground
     "
   >
-    <HomeHero />
-    <HomeLogos />
-    <HomeFeatures />
-    <HomeStats />
-    <HomeTestimonials />
-    <HomeCta />
-  </div>
+    <div class="flex max-w-xl flex-col items-center text-center">
+      <img
+        src="/straun.svg"
+        :alt="`${title} logo`"
+        width="96"
+        height="96"
+        class="
+          size-20
+          md:size-24
+        "
+      >
+      <h1
+        class="
+          mt-8 text-4xl font-black tracking-tight text-balance
+          md:text-5xl
+        "
+      >
+        {{ title }}
+      </h1>
+      <p class="mt-4 text-lg text-pretty text-muted-foreground">
+        {{ description }}
+      </p>
+      <Button
+        as-child
+        size="lg"
+        class="mt-10"
+      >
+        <NuxtLink to="/dashboard">
+          <span class="text-nowrap">Open Dashboard</span>
+        </NuxtLink>
+      </Button>
+    </div>
+  </section>
 </template>
